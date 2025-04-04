e14252b5b53eb6a4e0bb8f4e4a76b1133315e86c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 11:45:58 -0300

    test

--- ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/service/PagamentoServiceIT.java
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/service/PagamentoServiceIT.java
@@ -1,23 +1,21 @@
 package com.github.cidarosa.ms_pagamento.service;
 
-
 import com.github.cidarosa.ms_pagamento.repository.PagamentoRepository;
 import com.github.cidarosa.ms_pagamento.service.exceptions.ResourceNotFoundException;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
-@SpringBootTest
+@SpringBootTest // carrega o contexto
+@Transactional // Rollback no DB
 
-    public class PagamentoServiceIT {
+public class PagamentoServiceIT {
 
     @Autowired
     private PagamentoService service;
-
     @Autowired
     private PagamentoRepository repository;
 
@@ -26,37 +24,38 @@ import org.springframework.transaction.annotation.Transactional;
     private Long countTotalPagamento;
 
     @BeforeEach
-    void setud() throws Exception {
+    void setup() throws Exception {
         existingId = 1L;
         nonExistingId = 100L;
         countTotalPagamento = 6L;
-
     }
 
     @Test
-    public void deletePagamentoShouldDeleteResourceWhenIdExists() {
+    public void deletePagamentoShouldDoNotingWhenIdExists() {
+
         service.deletePagamento(existingId);
         Assertions.assertEquals(countTotalPagamento - 1, repository.count());
     }
 
     @Test
     public void deletePagamentoShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
-        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-            service.deletePagamento(nonExistingId);
-        });
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> {
+                    service.deletePagamento(nonExistingId);
+                });
     }
     @Test
     public void getAllShouldReturnListPagamentoDTO() {
-        var result = service.getAll();
 
+        var result = service.getAll();
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals(countTotalPagamento, result.size());
         Assertions.assertEquals(Double.valueOf(35.55), result.get(0).getValor().doubleValue());
         Assertions.assertEquals("Amadeus Mozart", result.get(0).getNome());
         Assertions.assertEquals("Chiquinha Gonzaga", result.get(1).getNome());
         Assertions.assertNull(result.get(5).getNome());
-        // ou Assertions.assertEquals(null, result.get(5).getNome());
+        Assertions.assertEquals(null, result.get(5).getNome());
+
     }
-
-
 }
