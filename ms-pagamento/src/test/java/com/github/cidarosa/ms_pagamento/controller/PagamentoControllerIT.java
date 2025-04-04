e1ed250e5721e0339dc7274c0684d54d01160836
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 11:51:11 -0300

    fix: correção do nome da classe PagamentoControllerIT

--- ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/controller/PagamentoControllerIT.java
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/controller/PagamentoControllerIT.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class PagamentoContrllerIT {
+public class PagamentoControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
